a422412d378f1d418e9ca2f09a9b1f7cc915adcc
dragonsourced <dragonsourced@protonmail.com>
2020-02-18 23:48:01 -0500

    Better font size.

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Nerd Font:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Iosevka Nerd Font:size=10";
 static int borderpx = 20;
 
 /*
